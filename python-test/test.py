d775114fbe69f2fadd3436e2247477e94da6ed13
zenggongcangfeng <jike201201@gmail.com>
2024-04-15 22:04:47 +0800

    测试

--- python-test/test.py
+++ python-test/test.py
@@ -1,7 +1,16 @@
+import pandas as pd
+
+# 创建示例数据框
+data = {
+    'category': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
+    'value': [10, 15, 20, 25, 20, 30, 35]
+}
+df = pd.DataFrame(data)
+
+# 使用 pandas 的 rank 方法对数据进行排名
+df['rank'] = df.groupby('category')['value'].rank(method='min', ascending=False)
+
+print(df)
 
 
-def print_lyrics():
-    #打印歌词
-    print("hello world")
-#调用函数print_lyrics
-print_lyrics()
+
