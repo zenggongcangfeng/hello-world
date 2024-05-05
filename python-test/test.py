70b213a71c726b7f2fda20de2895eca6d24b60ed
zenggongcangfeng <jike201201@gmail.com>
2024-05-05 19:58:21 +0800

    update

--- python-test/test.py
+++ python-test/test.py
@@ -1,16 +1,32 @@
-import pandas as pd
+import requests
+import re
 
-# 创建示例数据框
-data = {
-    'category': ['A', 'A', 'B', 'B', 'B', 'C', 'C'],
-    'value': [10, 15, 20, 25, 20, 30, 35]
-}
-df = pd.DataFrame(data)
+def get_one_page(url):
+    # 构造请求头
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+    }
+    
+    
+    # 发送GET请求
+    response = requests.get(url, headers=headers)
+    
+    #判断请求是否正常
+    if response.status_code == 200:
+        return response.text
+    return None
 
-# 使用 pandas 的 rank 方法对数据进行排名
-df['rank'] = df.groupby('category')['value'].rank(method='min', ascending=False)
+def parse_one_page(html):
+    # 解析网页内容
+    # ...
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    print(items)
 
-print(df)
+if __name__ == '__main__':
+    url = 'https://www.maoyan.com/board/4'
+    html = get_one_page(url)
+    parse_one_page(html)
+    
 
-
-
+   
